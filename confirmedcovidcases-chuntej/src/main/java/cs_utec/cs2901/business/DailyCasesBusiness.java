a5cde6f24e5528573436418ea00ee059a54a659d
Fgarcia19 <fabrizio.garcia@utec.edu.pe>
2020-04-03 09:10:53 -0500

    Controller

---
+++ confirmedcovidcases-chuntej/src/main/java/cs_utec/cs2901/business/DailyCasesBusiness.java
@@ -0,0 +1,67 @@
+package cs_utec.cs2901.business;
+
+import cs_utec.cs2901.repositories.DailyCasesRepository;
+import cs_utec.cs2901.data.DailyCases;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class DailyCasesBusiness
+{
+  @Autowired
+  private DailyCasesRepository repository;
+
+  /*
+    CREATE guarda un nuevo DailyCase
+    @return DailyCase guardado
+  */
+
+  public DailyCases create(DailyCases newDailyCase)
+  {
+    return repository.save(newDailyCase);
+  }
+
+  /*
+    Obtiene todos los daily cases
+    return dailycases
+  */
+
+  public List<DailyCases> findAll()
+  {
+    List<DailyCases>items = new ArrayList<>();
+
+    for(DailyCases item: repository.findAll())
+    {
+        items.add(item);
+    }
+
+
+    return items;
+  }
+
+  /*
+    Actualiza un dailycase existente
+    return: DailyCase Actualizado
+  */
+
+  public DailyCases update(DailyCases item)
+  {
+    return repository.save(item);
+  }
+
+
+  /*
+    Elimina un dailycase existente
+  */
+  public void delete(Long id)
+  {
+    repository.deleteById(id);
+  }
+
+
+
+}
